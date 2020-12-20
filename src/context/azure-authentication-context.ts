fea96d5eb289f0ba56fedc35e10188c965ac9ba4
icarus96119 <icarus96119@gmail.com>
2020-12-20 05:47:13 -0800

    remove: redundant code

--- src/context/azure-authentication-context.ts
+++ src/context/azure-authentication-context.ts
@@ -1,27 +1,18 @@
-import {
-  PublicClientApplication,
-  AuthenticationResult,
-  AccountInfo,
-  EndSessionRequest,
-  RedirectRequest,
-  PopupRequest,
-} from "@azure/msal-browser";
+import { AccountInfo, PopupRequest, PublicClientApplication, RedirectRequest, } from "@azure/msal-browser";
 
-import { logInPopUp, loginRedirect, MSAL_CONFIG } from '../environment';
+import { MSAL_CONFIG } from '../environment';
 
 export class AzureAuthenticationContext {
 
   private msalObject: PublicClientApplication = new PublicClientApplication(
     MSAL_CONFIG
   );
-  private account?: AccountInfo | null | undefined;
   private loginRedirectRequest?: RedirectRequest;
   private loginRequest?: PopupRequest;
 
   public isAuthenticationConfigured = false;
 
   constructor() {
-    this.account = undefined;
     this.setRequestObjects();
     if (MSAL_CONFIG?.auth?.clientId) {
       this.isAuthenticationConfigured = true;
@@ -40,55 +31,10 @@ export class AzureAuthenticationContext {
     };
   }
 
-  login(signInType: string, setUser: any): void {
-    if (signInType === logInPopUp) {
-      this.msalObject
-        .loginPopup(this.loginRequest)
-        .then((resp: AuthenticationResult) => {
-          this.handleResponse(resp, setUser);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    } else if (signInType === loginRedirect) {
-      this.msalObject.loginRedirect(this.loginRedirectRequest);
-    }
+  login(): void {
+    this.msalObject.loginRedirect(this.loginRedirectRequest);
   }
 
-  logout(account: AccountInfo): void {
-    const logOutRequest: EndSessionRequest = {
-      account,
-    };
-
-    this.msalObject.logout(logOutRequest);
-  }
-  handleResponse(response: AuthenticationResult, incomingFunction: any) {
-    if (response) {
-      this.account = response.account;
-    } else {
-      this.account = this.getAccount();
-    }
-
-    if (this.account) {
-      incomingFunction(this.account);
-    }
-  }
-
-  private getAccount(): AccountInfo | undefined {
-    console.log('loadAuthModule');
-    const currentAccounts = this.msalObject.getAllAccounts();
-    if (!currentAccounts) {
-      console.log('No accounts detected');
-      return undefined;
-    }
-
-    if (currentAccounts.length > 1) {
-      console.log('Multiple accounts detected, need to add choose account code.');
-      return currentAccounts[0];
-    } else if (currentAccounts.length === 1) {
-      return currentAccounts[0];
-    }
-  }
 }
 
 export default AzureAuthenticationContext;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,36 +1,12 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { AccountInfo } from "@azure/msal-browser";
 import Button from '@atlaskit/button';
 
 import AzureAuthenticationContext from '../context/azure-authentication-context';
-import { setAuthToken } from '../redux/actions';
-import { logInPopUp, loginRedirect } from '../environment';
 
 export default function LogIn() {
   const authenticationModule: AzureAuthenticationContext = new AzureAuthenticationContext();
-  const dispatch = useDispatch();
-
-  const userAgent = window.navigator.userAgent;
-  const msie = userAgent.indexOf("MSIE ");
-  const msie11 = userAgent.indexOf("Trident/");
-  const isIE = msie > 0 || msie11 > 0;
-
-  const returnedAccountInfo = (user: AccountInfo) => {
-    // set state
-    if (user) {
-      dispatch(setAuthToken(user.homeAccountId));
-    }
-  };
-
-  const logIn = (method: string): any => {
-    const logInType = isIE ? loginRedirect : logInPopUp;
-
-    // Azure Login
-    authenticationModule.login(logInType, returnedAccountInfo);
-  };
 
   return (<div className="h-screen flex justify-center items-center">
-    <Button id="authenticationButton" onClick={() => logIn("loginPopup")}>Log in</Button>
+    <Button id="authenticationButton" onClick={authenticationModule.login}>Log in</Button>
   </div>);
 }
